849d9e76a899d7c2981bd5d2fa362f2d1f30eafd
El-Mabe <julian.mabesoy@correo.icesi.edu.co>
2020-05-24 14:51:54 -0500

    se creo el proyecyo en C#

---
+++ Proyecto-Final-ArquiHard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto_Final_ArquiHard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
