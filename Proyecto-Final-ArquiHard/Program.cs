65ae9281e4c00042c71012ea023f2906ffd1ac0c
El-Mabe <julian.mabesoy@correo.icesi.edu.co>
2020-05-24 20:33:54 -0500

    se añadieron las versiones

--- Proyecto-Final-ArquiHard/Program.cs
+++ Proyecto-Final-ArquiHard/Program.cs
@@ -62,7 +62,195 @@ namespace Proyecto_Final_ArquiHard
             return imagenYTiempo;
         }
 
-        
+        public static object[] version2(Bitmap imagenEnBitmap, String numeroDeBits)
+        {
+
+            object[] imagenYTiempo = new object[2];
+
+            //Medicion de tiempo
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            sw.Start();
+           
+            for (int x = 0; x < imagenEnBitmap.Width; x++)
+            {
+                for (int y = 0; y < imagenEnBitmap.Height; y++)
+                {
+                    Color c = imagenEnBitmap.GetPixel(x, y);
+                    Color nuevo = Color.FromArgb(255 - c.R, c.G, c.B);
+                    imagenEnBitmap.SetPixel(x, y, nuevo);
+                }
+            }
+
+            for (int x = 0; x < imagenEnBitmap.Width; x++)
+            {
+                for (int y = 0; y < imagenEnBitmap.Height; y++)
+                {
+                    Color c = imagenEnBitmap.GetPixel(x, y);
+                    Color nuevo = Color.FromArgb(c.R, 255 - c.G, c.B);
+                    imagenEnBitmap.SetPixel(x, y, nuevo);
+                }
+            }
+
+            for (int x = 0; x < imagenEnBitmap.Width; x++)
+            {
+                for (int y = 0; y < imagenEnBitmap.Height; y++)
+                {
+                    Color c = imagenEnBitmap.GetPixel(x, y);
+                    Color nuevo = Color.FromArgb(c.R, c.G, 255 - c.B);
+                    imagenEnBitmap.SetPixel(x, y, nuevo);
+                }
+            }
+            sw.Stop();
+
+
+            long tiempo = (long)(sw.Elapsed.TotalMilliseconds * 1000000);
+            Image nueva = (Image)imagenEnBitmap;
+            imagenYTiempo[0] = nueva;
+            imagenYTiempo[1] = tiempo;
+
+            /*
+            imagenEnBitmap.Save("C:/Users/Principal/Desktop/imagenes/v2.jpg");
+            Console.WriteLine("fin 2");
+            */
+            //end
+            
+            /*
+            String ruta = "";
+            Bitmap bm3 = new Bitmap(Image.FromFile(ruta));
+            */
+            return imagenYTiempo;
+        }
+
+
+        public static Object[] version3(Bitmap imagenEnBitmap, String numeroDeBits)
+        {
+            Object[] imagenYTiempo = new Object[2];
+
+
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            sw.Start();
+            //version 3
+            for (int x = 0; x < imagenEnBitmap.Height; x++)
+            {
+                for (int y = 0; y < imagenEnBitmap.Width; y++)
+                {
+                    Color c = imagenEnBitmap.GetPixel(y, x);
+                    Color nuevo = Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                    imagenEnBitmap.SetPixel(y, x, nuevo);
+                }
+            }
+            sw.Stop();
+
+            long tiempo = (long)(sw.Elapsed.TotalMilliseconds * 1000000);
+            Image nueva = (Image)imagenEnBitmap;
+            imagenYTiempo[0] = nueva;
+            imagenYTiempo[1] = tiempo;
+
+            /*
+            imagenEnBitmap.Save("C:/Users/Principal/Desktop/imagenes/v3.jpg");
+            Console.WriteLine("fin 3");
+            //end
+
+            Bitmap bm4 = new Bitmap(Image.FromFile(ruta));
+            */
+
+
+            return imagenYTiempo;
+        }
+
+        public static Object version4(Bitmap imagenEnBitmap, String numeroDeBits)
+        {
+            Object[] imagenYTiempo = new Object[2];
+
+
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            sw.Start();
+            //version 4
+            for (int x = 0; x < imagenEnBitmap.Width; x++)
+            {
+                for (int y = 0; y < imagenEnBitmap.Height; y++)
+                {
+                    Color c = imagenEnBitmap.GetPixel(x, y);
+                    Color nuevo = Color.FromArgb(255 - c.R, c.G, c.B);
+                    imagenEnBitmap.SetPixel(x, y, nuevo);
+                }
+            }
+
+            for (int x = imagenEnBitmap.Width - 1; x > -1; x--)
+            {
+                for (int y = imagenEnBitmap.Height - 1; y > -1; y--)
+                {
+                    Color c = imagenEnBitmap.GetPixel(x, y);
+                    Color nuevo = Color.FromArgb(c.R, 255 - c.G, 255 - c.B);
+                    imagenEnBitmap.SetPixel(x, y, nuevo);
+                }
+            }
+            sw.Stop();
+
+            long tiempo = (long)(sw.Elapsed.TotalMilliseconds * 1000000);
+            Image nueva = (Image)imagenEnBitmap;
+            imagenYTiempo[0] = nueva;
+            imagenYTiempo[1] = tiempo;
+            /*
+            imagenEnBitmap.Save("C:/Users/Principal/Desktop/imagenes/v4.jpg");
+            Console.WriteLine("fin 4");
+            //end
+
+
+            Bitmap bm5 = new Bitmap(Image.FromFile(ruta));
+            */
+            return imagenYTiempo;
+        }
+
+        public static Object version5(Bitmap imagenEnBitmap, String numeroDeBits)
+        {
+            Object[] imagenYTiempo = new Object[2];
+
+
+            Stopwatch sw = new Stopwatch();
+            sw.Restart();
+            sw.Start();
+
+            //version 5
+
+            for (int x = 0; x < imagenEnBitmap.Width - 1; x += 2)
+            {
+                for (int y = 0; y < imagenEnBitmap.Height - 1; y += 2)
+                {
+                    Color c = imagenEnBitmap.GetPixel(x, y);
+                    Color nuevo = Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                    imagenEnBitmap.SetPixel(x, y, nuevo);
+
+                    c = imagenEnBitmap.GetPixel(x, y + 1);
+                    nuevo = Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                    imagenEnBitmap.SetPixel(x, y + 1, nuevo);
+
+                    c = imagenEnBitmap.GetPixel(x + 1, y);
+                    nuevo = Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                    imagenEnBitmap.SetPixel(x + 1, y, nuevo);
+
+                    c = imagenEnBitmap.GetPixel(x + 1, y + 1);
+                    nuevo = Color.FromArgb(255 - c.R, 255 - c.G, 255 - c.B);
+                    imagenEnBitmap.SetPixel(x + 1, y + 1, nuevo);
+                }
+            }
+            sw.Stop();
+
+            long tiempo = (long)(sw.Elapsed.TotalMilliseconds * 1000000);
+            Image nueva = (Image)imagenEnBitmap;
+            imagenYTiempo[0] = nueva;
+            imagenYTiempo[1] = tiempo;
+
+            /*
+            imagenEnBitmap.Save("C:/Users/Principal/Desktop/imagenes/v5.jpg");
+            Console.WriteLine("fin 5");
+            //end
+            */
+            return imagenYTiempo;
+        }
 
 
     }
